7909d755604c16a0da48d845122d0cba8d3f0e92
Ashwini <ashwinilakshmanan1@gmail.com>
2022-11-13 20:35:12 +0530

    products by id

--- src/main/java/in/upcode/eshop/controller/ProductController.java
+++ src/main/java/in/upcode/eshop/controller/ProductController.java
@@ -4,6 +4,8 @@ import in.upcode.eshop.services.ProductService;
 import model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -18,5 +20,11 @@ public class ProductController {
     @GetMapping("/products")
     public List<Product>getAllProducts(){
         return productService.getAllProducts();
+    }
+
+    @RequestMapping("/products/{id}")
+    public Product getProduct(@PathVariable int id){
+        return  productService.getProduct(id);
+
     }
 }
--- src/main/java/in/upcode/eshop/services/ProductService.java
+++ src/main/java/in/upcode/eshop/services/ProductService.java
@@ -4,19 +4,24 @@ import model.Product;
 import org.springframework.stereotype.Service;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class ProductService {
-private List<Product> products= Arrays.asList(
+private List<Product> products= new ArrayList<>(Arrays.asList(
         new Product(1, "Shoes", 10),
         new Product(2, "Books", 100),
         new Product(3, "Electronics", 200)
-);
+));
 
 public List<Product>getAllProducts(){
     return products;
+}
+
+public Product getProduct(int id){
+    return products.stream().filter(t->t.getId()==id).findFirst().get();
 }
 
 }
